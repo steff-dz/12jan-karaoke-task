b01c508d4cd0e6c7e5ce36a90694a042f53fa2aa
Steff <stefaniemarie@gmail.com>
2021-01-13 17:35:40 +0100

    more styling and set up some states

--- src/containers/HomeContainer/index.jsx
+++ src/containers/HomeContainer/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const HomeContainer = () => {
+    const [artist, setArtist] = useState('')
+    const [song, setSong] = useState('')
+    const [responseData, setResponseData] = useState('')
+    
 	return (
-		<main>
+		<>
 			<PageHeader>
 				<h1>Steff's Karaoke Machine</h1>
 			</PageHeader>
-		</main>
+            <main>
+                <FormContainer>
+                    <label htmlFor="artist">Artist:</label>
+                    <input type="text" placeholder="type artist name here"/>
+                    <label htmlFor="song">Song:</label>
+                    <input type="text" placeholder="type song title here"/>
+                    <button type="submit">Let's Sing!</button>
+                </FormContainer>
+           </main>
+		</>
 	);
 };
 
-const PageHeader = styled.h1`
+const PageHeader = styled.header`
 	color: white;
 	text-align: center;
 
@@ -19,5 +32,30 @@ const PageHeader = styled.h1`
 		font-size: 5rem;
 	}
 `;
+
+const FormContainer = styled.form`
+border: 1px solid black;
+width: 90vw;
+margin: 0 auto;
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+gap: 1rem;
+padding: 1rem;
+label {
+    font-size: 2rem;
+    font-weight: bold;
+    color: white;
+}
+
+input {
+    width: 300px;
+}
+
+button {
+    font-size: 2rem;
+    padding: .2rem;
+}
+`
 
 export default HomeContainer;
