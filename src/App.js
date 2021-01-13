9106acd96d9daf9e717f3ed1dcc25af263683c4d
Steff <stefaniemarie@gmail.com>
2021-01-13 16:48:32 +0100

    initial commits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContainer from './containers/HomeContainer'
+
+function App() {
+  return (
+    <>
+     <HomeContainer/>
+    </>
+  )
+};
+
+export default App;
---
+++ src/containers/HomeContainer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomeContainer = () => {
+	return (
+		<main>
+			<header>
+				<h1>Steff's Karaoke Machine</h1>
+			</header>
+		</main>
+	);
+};
+
+export default HomeContainer;
